a59cf5095e94727b56a3b2d110428bd283777c37
YifangZhang <yifangzhang2009@gmail.com>
2015-07-01 22:52:17 -0500

    initalize

---
+++ testClasses/testClasses/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testClasses
+//
+//  Created by Yifang Zhang on 7/1/15.
+//  Copyright (c) 2015 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testClasses/testClasses/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testClasses
+//
+//  Created by Yifang Zhang on 7/1/15.
+//  Copyright (c) 2015 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
