e599866ee3868a9707036fbd576af1d24395a139
YifangZhang <yifangzhang2009@gmail.com>
2015-07-03 00:32:57 -0500

    db manager and shared model init

---
+++ testClasses/testClasses/SharedDatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  SharedDatabaseManager.h
+//  testClasses
+//
+//  Created by Yifang Zhang on 7/2/15.
+//  Copyright (c) 2015 Yifang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SharedDatabaseManager : NSObject
+
+@end
---
+++ testClasses/testClasses/SharedDatabaseManager.m
@@ -0,0 +1,13 @@
+//
+//  SharedDatabaseManager.m
+//  testClasses
+//
+//  Created by Yifang Zhang on 7/2/15.
+//  Copyright (c) 2015 Yifang. All rights reserved.
+//
+
+#import "SharedDatabaseManager.h"
+
+@implementation SharedDatabaseManager
+
+@end
